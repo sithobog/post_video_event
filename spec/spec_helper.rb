bd336d5baa271cd8c03a7c0cdadd2409c318c537
Istomin Bohdan <sithobog@ymail.com>
2015-10-03 15:11:43 +0300

    init commit

---
+++ spec/spec_helper.rb
@@ -0,0 +1,29 @@
+# See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+RSpec.configure do |config|
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+ 
+  config.before(:suite) do 
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean
+  end
+
+  config.after(:all) do
+    DatabaseCleaner.clean
+  end
+
+
+
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+end
