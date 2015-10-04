bfa2c9ce4a21400c6ab7f2c16145abb049101811
Istomin Bohdan <sithobog@ymail.com>
2015-10-04 22:48:19 +0300

    update views and css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+  	@posts = Post.last(3).reverse
+  	@videos = Video.last(3).reverse
+  	@events = Event.last(3).reverse
   end
 end
--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -6,6 +6,10 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   process resize_to_fit: [500, 500]
 
+  version :thumb do
+    process resize_to_fill: [150,150]
+  end
+
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
