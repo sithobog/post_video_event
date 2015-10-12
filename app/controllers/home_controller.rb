ca5647dd5bc70db55e96ebc3571cb463eb979ad6
Istomin Bohdan <sithobog@ymail.com>
2015-10-12 23:15:24 +0300

    optimize process of finding tags for tags cloud

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,16 +8,35 @@ class HomeController < ApplicationController
 
   private
     def cloud
+      @tag_hash = Hash.new
       @array_for_render = Array.new
       @tags = Tag.all
+
+      # create keys from tags id
+      #
       @tags.each do |tag|
-        tag_count = ContentView.any_tags(tag.id).count
-        @array_for_render <<{
+        @tag_hash[tag.id] = 0
+      end
+
+      # counting tags
+      #
+      ContentView.all.each do |article|
+        article.tag_ids.each do |tag|
+            @tag_hash[tag] +=1
+        end
+      end
+
+      # create array for JQCloud
+      @tags.each do |tag|
+        @array_for_render << {
           text: tag.name,
-          weight: tag_count,
+          weight: @tag_hash[tag.id],
           link: "tags/#{tag.slug}"
         }
       end
+      # create variable for JS
+      #
       gon.from_home_controller = @array_for_render
+
     end
 end
